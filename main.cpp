b15fccc6ba64ea819aa531552f34a0c1a50a4dfd
sims2010wow <36426715+sims2010wow@users.noreply.github.com>
2018-04-04 20:18:13 -0400

    Update main.cpp
    update

--- main.cpp
+++ main.cpp
@@ -8,17 +8,45 @@
 void display(void);
 float input(int);
 int choice(void);
+void math(int will1,float num1,float num2);
 
 int main(void) {
 	float num1,num2;
 	int will1;
+	char will2;
 	display();
 	num1 = input(1);
 	printf("\n\n");
 	num2 = input(2);
-	will1 = choice();
+	do {
+		will1 = choice();
+		math(will1, num1, num2);
+		printf("\nWould you like to use these numbers again (y/n)? ");
+		scanf(" %c", &will2);
+	} while(will2 == 'y');
 	system("pause");
 }
+
+void math(int will1, float num1, float num2) {
+	char will2;
+
+		switch (will1) {
+		case 1:
+			printf("The sum %0.2f+%0.2f=%0.2f\n", num1, num2, num1 + num2);
+			break;
+		case 2:
+			printf("The difference %0.2f-%0.2f=%0.2f\n", num1, num2, num1 - num2);
+			break;
+		case 3:
+			printf("The product %0.2f*%0.2f=%0.2f\n", num1, num2, num1 * num2);
+			break;
+		case 4:
+			printf("The quotient %0.2f/%0.2f=%0.2f\n", num1, num2, num1 / num2);
+			break;
+		}
+}
+
+
 int choice(void) {
 	int choice;
 	printf("\n\nSelect operation to preform on two numbers entered.\n\n\
@@ -26,7 +54,7 @@ int choice(void) {
 Enter interger values for choice (1,2,3,4): ");
 	scanf(" %d", &choice);
 	while (1) {
-		if (choice != 1 || choice != 2 || choice != 3 || choice != 4) {
+		if (choice>4 || choice<1) {
 			printf("ERROR: You must enter 1, 2, 3 or 4!\n\n\
 please try again: ");
 			scanf(" %d", &choice);
